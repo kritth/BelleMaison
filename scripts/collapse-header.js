77a8b8716c08b30545feb2d8b595911ee4f99f7e
kritth <rkasidis@gmail.com>
2016-08-27 17:48:55 -0400

    Updated animation for index.html
    - Add glass door animation to home section
    - Add parallax effect to most sections

--- scripts/collapse-header.js
+++ scripts/collapse-header.js
@@ -1,9 +1,9 @@
 $(document).scroll(function (e) {
-    if (document.body.scrollTop > 0 && document.body.scrollTop < 200) {
-		$('.top-menu').css({'top': -1 * document.body.scrollTop / 6 + 'px'});
+    if (document.body.scrollTop > 0 && document.body.scrollTop < 800) {
+		$('.top-menu').css({'top': -1 * document.body.scrollTop / 22 + 'px'});
     } else if (document.body.scrollTop <= 0) {
 		$('.top-menu').css({'top': '0px'});
-	} else if (document.body.scrollTop >= 200) {
+	} else if (document.body.scrollTop >= 800) {
 		$('.top-menu').css({'top': '-35px'});
 	}
 });
